9992173411b0f28d8bde2c7339569a7cc84c7f01
Oleg Kireev <okireev@ubic.tech>
2022-10-28 11:46:02 +0400

    @/app/Layout: remove 'header' and 'footer' titles

--- src/app/Layout/index.tsx
+++ src/app/Layout/index.tsx
@@ -9,14 +9,10 @@ export const Layout = ({
   children,
 }: LayoutProps) => (
   <div className={styles.wrapper}>
-    <header className={styles.header}>
-      header
-    </header>
+    <header className={styles.header} />
     <main className={styles.main}>
       {children}
     </main>
-    <footer className={styles.footer}>
-      footer
-    </footer>
+    <footer className={styles.footer} />
   </div>
 );
