1829af76f14349c44857efc513ec7bb4a5cd8a76
Oleg Kireev <okireev@ubic.tech>
2022-10-28 11:46:20 +0400

    @/app/App: put TalentCalc into Layout

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -1,10 +1,11 @@
+import { TalentCalc } from 'features/talent-calc';
 import { Layout } from '../Layout';
 import styles from './styles.module.scss';
 
 export const App = () => (
   <div className={styles.wrapper}>
     <Layout>
-      Wow-talent-calc
+      <TalentCalc />
     </Layout>
   </div>
 );
