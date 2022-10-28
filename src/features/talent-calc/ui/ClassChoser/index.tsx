a0c4fb9099a3c3042b377b1dd13319567e32729d
Oleg Kireev <okireev@ubic.tech>
2022-10-28 12:54:41 +0400

    @/features/talent-calc/ui/ClassChoser: implement basic logic

--- src/features/talent-calc/ui/ClassChoser/index.tsx
+++ src/features/talent-calc/ui/ClassChoser/index.tsx
@@ -1,5 +1,26 @@
-export const ClassChoser = () => (
-  <div>
-    ClassChoser
+import { characterClasses, CharacterClassUnion } from 'shared/constants/global';
+import { AbilityButton } from 'shared/ui/AbilityButton';
+import styles from './styles.module.scss';
+
+interface ClassChoserProps {
+  currentClass: CharacterClassUnion | null,
+  onClassChange: (newClass: CharacterClassUnion) => void,
+}
+
+export const ClassChoser = ({
+  currentClass,
+  onClassChange,
+}: ClassChoserProps) => (
+  <div className={styles.wrapper}>
+    {characterClasses.map((characterClass) => (
+      <AbilityButton
+        key={characterClass}
+        isActive={characterClass === currentClass}
+        isDisabled={false}
+        onClick={() => onClassChange(characterClass)}
+      >
+        {characterClass}
+      </AbilityButton>
+    ))}
   </div>
 );
