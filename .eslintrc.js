b07db21d51cae62c6c9618dde050e56995506b52
Oleg Kireev <okireev@ubic.tech>
2022-10-28 10:39:31 +0400

    package: install eslint, sass

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb/whitespace',
+    'airbnb-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+  }
+}
