54cb89face36d48b4703fbef5f3219757ea4a322
Oleg Kireev <okireev@ubic.tech>
2022-10-31 19:51:18 +0400

    @/shared/constants/talentsData: add ids

--- src/shared/constants/talentsData.ts
+++ src/shared/constants/talentsData.ts
@@ -10,6 +10,7 @@ export type AbilityRange = number;
 
 export type TalentType = {
   title: string,
+  id: string,
   // TODO: make dependence of 'max'
   description: {
     1: string,
@@ -43,6 +44,7 @@ export const talentsData: TalentsType = {
     title: 'frost',
     backgroundImage: 'https://wow.zamimg.com/images/wow/talents/backgrounds/wrath/61.jpg',
     talents: [{
+      id: 'mage_frost_frostbite',
       title: 'Frostbite',
       description: {
         1: 'Gives your Chill effects a 5% chance to freeze the target for 5 sec.',
@@ -54,6 +56,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 3,
     }, {
+      id: 'mage_frost_imporved_frostbolt',
       title: 'Improved Frostbolt',
       description: {
         1: 'Reduces the casting time of your Frostbolt spell by 0.1 sec.',
@@ -67,6 +70,7 @@ export const talentsData: TalentsType = {
       column: 2,
       max: 5,
     }, {
+      id: 'mage_frost_ice_floes',
       title: 'Ice Floes',
       description: {
         1: 'Reduces the cooldown of your Frost Nova, Cone of Cold, Ice Block and Icy Veins spells by 7%.',
@@ -78,6 +82,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_ice_shards',
       title: 'Ice Shards',
       description: {
         1: 'Increases the critical strike damage bonus of your Frost spells by 33%.',
@@ -89,6 +94,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 3,
     }, {
+      id: 'mage_frost_fronst_warding',
       title: 'Frost Warding',
       description: {
         1: 'Increases the armor and resistances given by your Frost Armor and Ice Armor spells by 25%. &nbsp;In addition, gives your Frost Ward and Fire Ward a 15% chance to negate the warded damage spell and restore mana equal to the damage caused.',
@@ -99,6 +105,7 @@ export const talentsData: TalentsType = {
       column: 2,
       max: 2,
     }, {
+      id: 'mage_frost_precision',
       title: 'Precision',
       description: {
         1: 'Reduces the mana cost and increases your chance to hit with spells by 1%.',
@@ -110,6 +117,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_permafrost',
       title: 'Permafrost',
       description: {
         1: 'Increases the duration of your Chill effects by 2 sec, reduces the target\'s speed by an additional 4%, and reduces the target\'s healing received by 7%.',
@@ -121,6 +129,7 @@ export const talentsData: TalentsType = {
       column: 4,
       max: 3,
     }, {
+      id: 'mage_frost_piercing_ice',
       title: 'Piercing Ice',
       description: {
         1: 'Increases the damage done by your Frost spells by 2%.',
@@ -132,6 +141,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 3,
     }, {
+      id: 'mage_frost_icy_veins',
       title: 'Icy Veins',
       description: {
         1: 'Hastens your spellcasting, increasing spell casting speed by 20% and reduces the pushback suffered from damaging attacks while casting by 100%. &nbsp;Lasts 20 sec.',
@@ -144,6 +154,7 @@ export const talentsData: TalentsType = {
       castDuration: 'instant',
       costs: '3% of base mana',
     }, {
+      id: 'mage_frost_improved_blizzard',
       title: 'Improved Blizzard',
       description: {
         1: 'Adds a chill effect to your Blizzard spell. &nbsp;This effect lowers the target\'s movement speed by 25%. &nbsp;Lasts 1.50 sec',
@@ -155,6 +166,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_arctic_reach',
       title: 'Arctic Reach',
       description: {
         1: 'Increases the range of your Frostbolt, Ice Lance, Deep Freeze and Blizzard spells and the radius of your Frost Nova and Cone of Cold spells by 10%.',
@@ -165,6 +177,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 2,
     }, {
+      id: 'mage_frost_frost_channeling',
       title: 'Frost Channeling',
       description: {
         1: 'Reduces the mana cost of all spells by 4% and reduces the threat caused by your Frost spells by 4%.',
@@ -176,6 +189,7 @@ export const talentsData: TalentsType = {
       column: 2,
       max: 3,
     }, {
+      id: 'mage_frost_shatter',
       title: 'Shatter',
       description: {
         1: 'Increases the critical strike chance of all your spells against frozen targets by 17%.',
@@ -187,6 +201,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_cold_snap',
       title: 'Cold Snap',
       description: {
         1: 'Reduces the mana cost of all spells by 4% and reduces the threat caused by your Frost spells by 4%.',
@@ -198,6 +213,7 @@ export const talentsData: TalentsType = {
       cooldown: 480,
       castDuration: 'instant',
     }, {
+      id: 'mage_frost_improved_cone_of_cold',
       title: 'Improved Cone of Cold',
       description: {
         1: 'Increases the damage dealt by your Cone of Cold spell by 15%.',
@@ -209,6 +225,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_frozen_core',
       title: 'Frozen Core',
       description: {
         1: 'Reduces the damage taken from all spells by 2%.',
@@ -220,6 +237,7 @@ export const talentsData: TalentsType = {
       column: 4,
       max: 3,
     }, {
+      id: 'mage_frost_cold_as_ice',
       title: 'Cold as Ice',
       description: {
         1: 'Reduces the cooldown of your Cold Snap, Ice Barrier and Summon Water Elemental spells by 10%.',
@@ -230,6 +248,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 2,
     }, {
+      id: 'mage_frost_winters_chill',
       title: 'Winter\'s Chill',
       description: {
         1: 'Increases your chance to critically hit with Frostbolt by an additional 1% and gives your Frost damage spells a 33% chance to apply the Winter\'s Chill effect, which increases the chance spells will critically hit the target by 1% for 15 sec. &nbsp;Stacks up to 5 times.',
@@ -241,6 +260,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_shattered_barrier',
       title: 'Shattered Barrier',
       description: {
         1: 'Gives your Ice Barrier spell a 50% chance to freeze all enemies within 10 yds for 8 sec when it is destroyed.',
@@ -251,6 +271,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 2,
     }, {
+      id: 'mage_frost_ice_barrier',
       title: 'Ice Barrier',
       description: {
         1: 'Instantly shields you, absorbing 455 damage. &nbsp;Lasts 1 min. &nbsp;While the shield holds, spellcasting will not be delayed by damage.',
@@ -263,6 +284,7 @@ export const talentsData: TalentsType = {
       costs: '21% of base mana',
       castDuration: 'instant',
     }, {
+      id: 'mage_frost_arctic_winds',
       title: 'Arctic Winds',
       description: {
         1: 'Increases all Frost damage you cause by 1% and reduces the chance melee and ranged attacks will hit you by 1%.',
@@ -276,6 +298,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 5,
     }, {
+      id: 'mage_frost_emprowered_frostbolt',
       title: 'Empowered Frostbolt',
       description: {
         1: 'Increases the damage of your Frostbolt spell by an amount equal to 5% of your spell power and reduces the cast time by 0.1 sec.',
@@ -286,6 +309,7 @@ export const talentsData: TalentsType = {
       column: 2,
       max: 2,
     }, {
+      id: 'mage_frost_fingers_of_frost',
       title: 'Fingers of Frost',
       description: {
         1: 'Gives your Chill effects a 7% chance to grant you the Fingers of Frost effect, which treats your next 2 spells cast as if the target were Frozen. &nbsp;Lasts 15 sec.',
@@ -296,6 +320,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 2,
     }, {
+      id: 'mage_frost_brainfreeze',
       title: 'Brain Freeze',
       description: {
         1: 'Your Frost damage spells with chilling effects have a 5% chance to cause your next Fireball or Frostfire Bolt spell to be instant cast and cost no mana.',
@@ -307,6 +332,7 @@ export const talentsData: TalentsType = {
       column: 1,
       max: 3,
     }, {
+      id: 'mage_frost_summon_water_elemental',
       title: 'Summon Water Elemental',
       description: {
         1: 'Summon a Water Elemental to fight for the caster for 45 sec.',
@@ -319,6 +345,7 @@ export const talentsData: TalentsType = {
       costs: '16 % of base mana',
       castDuration: 'instant',
     }, {
+      id: 'mage_frost_enduring_winter',
       title: 'Enduring Winter',
       description: {
         1: 'Increases the duration of your Summon Water Elemental spell by 5 sec and your Frostbolt spell has a 33% chance to grant up to 10 party or raid members mana regeneration equal to 1% of their maximum mana per 5 sec for 15 sec. &nbsp;This effect cannot occur more often than once every 6 sec.',
@@ -330,6 +357,7 @@ export const talentsData: TalentsType = {
       column: 3,
       max: 3,
     }, {
+      id: 'mage_frost_chilled_to_the_bone',
       title: 'Chilled to the Bone',
       description: {
         1: 'Increases the damage caused by your Frostbolt, Frostfire Bolt and Ice Lance spells by 1% and reduces the movement speed of all chilled targets by an additional 2%.',
@@ -343,6 +371,7 @@ export const talentsData: TalentsType = {
       column: 2,
       max: 5,
     }, {
+      id: 'mage_frost_deep_freeze',
       title: 'Deep Freeze',
       description: {
         1: 'Increases the damage caused by your Frostbolt, Frostfire Bolt and Ice Lance spells by 5% and reduces the movement speed of all chilled targets by an additional 10%.',
