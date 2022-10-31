7a486003d0ed2bc5cf86d9e092024f964c664696
Oleg Kireev <okireev@ubic.tech>
2022-10-31 17:28:00 +0400

    @/features/talent-calc: pass currentClass to SpecializationTrees

--- src/features/talent-calc/index.tsx
+++ src/features/talent-calc/index.tsx
@@ -2,16 +2,20 @@ import { useTalentCalc } from './model/useTalentCalc';
 import { ClassChoser } from './ui/ClassChoser';
 import { Controls } from './ui/Controls';
 import { SpecializationTrees } from './ui/SpecializationTrees';
+import styles from './styles.module.scss';
 
 export const TalentCalc = () => {
   const { currentClass, onClassChange } = useTalentCalc();
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       <ClassChoser
         currentClass={currentClass}
         onClassChange={onClassChange}
       />
-      <SpecializationTrees />
+      <SpecializationTrees
+        currentClass={currentClass}
+      />
       <Controls />
     </div>
   );
