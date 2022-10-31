20fb55fcc8889ecd11158668da2ccbcec2963a22
Oleg Kireev <okireev@ubic.tech>
2022-10-31 19:47:29 +0400

    @/app/App: wrap with BrowserRouter

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -1,11 +1,18 @@
 import { TalentCalc } from 'features/talent-calc';
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
 import { Layout } from '../Layout';
 import styles from './styles.module.scss';
 
 export const App = () => (
   <div className={styles.wrapper}>
     <Layout>
-      <TalentCalc />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TalentCalc />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
   </div>
 );
