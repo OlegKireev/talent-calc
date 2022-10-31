38e316cd8fa3656dcfc2f2196b912e3099a66de8
Oleg Kireev <okireev@ubic.tech>
2022-10-31 17:27:28 +0400

    @/features/talent-calc/ui/SpecializationTrees: create

--- src/features/talent-calc/ui/SpecializationTrees/index.tsx
+++ src/features/talent-calc/ui/SpecializationTrees/index.tsx
@@ -1,5 +1,30 @@
-export const SpecializationTrees = () => (
-  <div>
-    SpecializationTrees
-  </div>
-);
+import { CharacterClassUnion } from 'shared/constants/global';
+import { talentsData } from 'shared/constants/talentsData';
+import { Tree } from './Tree';
+import styles from './styles.module.scss';
+
+interface SpecializationTreesProps {
+  currentClass: CharacterClassUnion | null
+}
+export const SpecializationTrees = ({
+  currentClass,
+}: SpecializationTreesProps) => {
+  if (!currentClass) {
+    return null;
+  }
+
+  const talentsByClass = talentsData[currentClass];
+
+  return (
+    <div className={styles.wrapper}>
+      {talentsByClass?.map((specializationData) => (
+        <Tree
+          title={specializationData.title}
+          talents={specializationData.talents}
+          backgroundImage={specializationData.backgroundImage}
+          key={specializationData.title}
+        />
+      ))}
+    </div>
+  );
+};
