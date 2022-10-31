112c467b54fc3ff03f858987319fb25e926c1a21
Oleg Kireev <okireev@ubic.tech>
2022-10-31 19:53:08 +0400

    @/features/talent-calc: create state

--- src/features/talent-calc/index.tsx
+++ src/features/talent-calc/index.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
+import { talentsData } from 'shared/constants/talentsData';
 import { useTalentCalc } from './model/useTalentCalc';
 import { ClassChoser } from './ui/ClassChoser';
 import { Controls } from './ui/Controls';
 import { SpecializationTrees } from './ui/SpecializationTrees';
 import styles from './styles.module.scss';
+import { createTalentsState } from './lib/transform';
+
+export type HandleTalentChangeArgs = { specialization: string, id: string, value: number };
 
 export const TalentCalc = () => {
   const { currentClass, onClassChange } = useTalentCalc();
+  const [talents, setTalents] = useState({});
+
+  useEffect(() => {
+    if (!currentClass) {
+      return;
+    }
+    setTalents(createTalentsState(talentsData[currentClass]!));
+  }, [currentClass]);
+
+  const handleTalentChange = ({
+    specialization,
+    id,
+    value,
+  }: HandleTalentChangeArgs) => {
+    setTalents((prev: { [key: typeof specialization]: any }) => ({
+      ...prev,
+      [specialization]: {
+        ...prev[specialization],
+        [id]: value,
+      },
+    }));
+  };
+
+  console.log(talents);
 
   return (
     <div className={styles.wrapper}>
@@ -15,6 +44,7 @@ export const TalentCalc = () => {
       />
       <SpecializationTrees
         currentClass={currentClass}
+        onTalentChange={handleTalentChange}
       />
       <Controls />
     </div>
