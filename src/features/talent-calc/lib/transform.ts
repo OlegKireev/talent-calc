5d8e4f749c1af6ab475eb179c38943f41b46fa5c
Oleg Kireev <okireev@ubic.tech>
2022-11-01 11:07:18 +0400

    @/features/talent-calc/lib/transform: add types

--- src/features/talent-calc/lib/transform.ts
+++ src/features/talent-calc/lib/transform.ts
@@ -1,9 +1,17 @@
+import { CharacterTalentId } from 'shared/constants/talents';
 import { TaletsOfClassType } from 'shared/constants/talentsData';
 
-export const createTalentsState = (obj: TaletsOfClassType[]) => obj.reduce((acc, cur) => ({
+export const createTalentsState = (
+  obj: TaletsOfClassType[],
+): TalentsDataReturn => obj.reduce((acc, cur) => ({
   ...acc,
   [cur.title]: cur.talents.reduce((innerAcc, innerCur) => ({
     ...innerAcc,
     [innerCur.id]: 0,
   }), {}),
 }), {});
+
+export type TalentsDataType = { [key in CharacterTalentId]?: number };
+export type TalentsDataReturn = {
+  [key: string]: TalentsDataType
+};
