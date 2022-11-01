0a1e46e9889fd007ab8e23f358ab70b23c07ef62
Oleg Kireev <okireev@ubic.tech>
2022-11-01 11:06:23 +0400

    @/shared/constants/talentsData: use CharacterTalentId type

--- src/shared/constants/talentsData.ts
+++ src/shared/constants/talentsData.ts
@@ -1,3 +1,4 @@
+import { CharacterTalentId } from './talents';
 import { CharacterClassUnion } from './global';
 
 export type TalentTierType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
@@ -10,7 +11,7 @@ export type AbilityRange = number;
 
 export type TalentType = {
   title: string,
-  id: string,
+  id: CharacterTalentId,
   // TODO: make dependence of 'max'
   description: {
     1: string,
