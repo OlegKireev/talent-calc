1e925c7fb6a8759a524d064a8833717fe40c0e7b
Oleg Kireev <okireev@ubic.tech>
2022-11-02 15:26:30 +0400

    @/shared/constants/global: create specs types

--- src/shared/constants/global.ts
+++ src/shared/constants/global.ts
@@ -1,13 +1,33 @@
 export const characterClasses = [
-  'warrior',
-  'hunter',
-  'rogue',
-  'mage',
-  'warlock',
-  'paladin',
-  'shaman',
   'deathknight',
-  'priest',
   'druid',
+  'hunter',
+  'mage',
+  'paladin',
+  'priest',
+  'rogue',
+  'shaman',
+  'warlock',
+  'warrior',
 ] as const;
-export type CharacterClassUnion = typeof characterClasses[number];
+export type CharacterClassType = typeof characterClasses[number];
+
+export const characteSpecs = [
+  'blood', 'frost', 'unholy', 'balance', 'feral', 'restoration', 'beast_mastery', 'marksmanship', 'survival', 'arcane', 'fire', 'frost', 'holy', 'protection', 'retribution', 'discipline', 'holy', 'shadow', 'assasination', 'combat', 'subtlety', 'elemental', 'enhancement', 'restoration', 'affliction', 'demonology', 'destruction', 'arms', 'fury', 'protection',
+] as const;
+export type CharacterSpecsType = typeof characteSpecs[number];
+
+export const specs: {
+  [key in CharacterClassType]: [CharacterSpecsType, CharacterSpecsType, CharacterSpecsType]
+} = {
+  deathknight: ['blood', 'frost', 'unholy'],
+  druid: ['balance', 'feral', 'restoration'],
+  hunter: ['beast_mastery', 'marksmanship', 'survival'],
+  mage: ['arcane', 'fire', 'frost'],
+  paladin: ['holy', 'protection', 'retribution'],
+  priest: ['discipline', 'holy', 'shadow'],
+  rogue: ['assasination', 'combat', 'subtlety'],
+  shaman: ['elemental', 'enhancement', 'restoration'],
+  warlock: ['affliction', 'demonology', 'destruction'],
+  warrior: ['arms', 'fury', 'protection'],
+};
