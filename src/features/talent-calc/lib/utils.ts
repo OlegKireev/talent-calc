99c2255af5cf7cf34b8b8ec495eaa9442a26d286
Oleg Kireev <okireev@ubic.tech>
2022-11-02 18:34:41 +0400

    @/features/talent-calc/lib/utils: add getDeepestTierWithValue, checkCanDecrease

--- src/features/talent-calc/lib/utils.ts
+++ src/features/talent-calc/lib/utils.ts
@@ -7,7 +7,7 @@ export const getTierTotal = (
   tier: TalentTierType,
   talents: TalentType[],
   data: TalentsDataType,
-) => {
+): number => {
   const tierTalentKeys = talents.reduce((acc: CharacterTalentIdType[], cur) => {
     if (cur.tier === tier) {
       return [...acc, cur.id];
@@ -28,7 +28,7 @@ export const getTierTotal = (
 export const checkIsTalentsDataRefreshed = (
   characterClass: CharacterClassType | null,
   talentsData: TalentsDataReturn,
-) => {
+): boolean => {
   if (!characterClass) {
     return false;
   }
@@ -38,3 +38,22 @@ export const checkIsTalentsDataRefreshed = (
     (key: any) => specs[characterClass].includes(key),
   );
 };
+
+export const getDeepestTierWithValue = (
+  template: TalentType[],
+  data: TalentsDataType,
+): number => {
+  const talentKeys = Object.keys(data) as CharacterTalentIdType[];
+  const talentKeysWithValue = talentKeys.filter((key) => Boolean(data[key]));
+
+  const talentsWithValue = template
+    .filter(({ id }) => talentKeysWithValue.includes(id));
+
+  return talentsWithValue.sort((a, b) => b.tier - a.tier)[0]?.tier || 0;
+};
+
+export const checkCanDecrease = (
+  deepestTier: number,
+  total: number,
+  tier: number,
+): boolean => (deepestTier - 1) * 5 <= total - 2 || tier === deepestTier;
