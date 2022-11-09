a57ed1635aa251b57c03033008b607a5e9890b2e
Oleg Kireev <okireev@ubic.tech>
2022-11-09 19:41:49 +0400

    @/shared/ui/Tooltip: display rank

--- src/shared/ui/Tooltip/index.tsx
+++ src/shared/ui/Tooltip/index.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './styles.module.scss';
 import { TooltipCoordsType, TooltipType } from './types';
+
+const MIN_RANK_VALUE = 1;
 
 interface TooltipProps {
   data: TooltipType,
@@ -12,8 +15,16 @@ export const Tooltip = ({
   coords,
 }: TooltipProps) => {
   const { title } = data;
+  const [rank, setRank] = useState<number>(1);
+
+  useEffect(() => {
+    if ('rank' in data) {
+      setRank(Math.max(data.rank, MIN_RANK_VALUE));
+    }
+  }, [data]);
 
   const { x, y } = coords;
+
   return createPortal(
     (
       <div
@@ -23,7 +34,18 @@ export const Tooltip = ({
           left: x,
         }}
       >
-        {title}
+        <header className={styles.header}>
+          <h3 className={styles.title}>
+            {title}
+          </h3>
+          {Boolean(rank) && (
+            <span className={styles.rank}>
+              Rank
+              {' '}
+              {rank}
+            </span>
+          )}
+        </header>
       </div>
     ),
     document.body,
