4b338758b8905d4ed572a1b05b0e7508120053bc
Oleg Kireev <okireev@ubic.tech>
2022-11-09 11:45:55 +0400

    @/features/talent-calc/ui/SpecializationTrees/Tree/Talent: temporary disable @typescript-eslint/no-unused-vars

--- src/features/talent-calc/ui/SpecializationTrees/Tree/Talent/index.tsx
+++ src/features/talent-calc/ui/SpecializationTrees/Tree/Talent/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { MouseEvent } from 'react';
 import cx from 'classnames';
 import { HandleTalentChangeArgs } from 'features/talent-calc';
