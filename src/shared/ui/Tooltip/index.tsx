e619766f3082022f34fdfba7e5847316f2eb30fd
Oleg Kireev <okireev@ubic.tech>
2022-11-09 18:31:55 +0400

    @/shared/ui/Tooltip: pass coords to dom element

--- src/shared/ui/Tooltip/index.tsx
+++ src/shared/ui/Tooltip/index.tsx
@@ -1,18 +1,28 @@
 import { createPortal } from 'react-dom';
 import styles from './styles.module.scss';
-import { TooltipType } from './types';
+import { TooltipCoordsType, TooltipType } from './types';
 
 interface TooltipProps {
   data: TooltipType,
+  coords: TooltipCoordsType,
 }
 
 export const Tooltip = ({
   data,
+  coords,
 }: TooltipProps) => {
   const { title } = data;
+
+  const { x, y } = coords;
   return createPortal(
     (
-      <div className={styles.wrapper}>
+      <div
+        className={styles.wrapper}
+        style={{
+          top: y,
+          left: x,
+        }}
+      >
         {title}
       </div>
     ),
