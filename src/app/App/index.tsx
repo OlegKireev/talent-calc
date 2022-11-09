6d5a1f80dd3ec227adb70396d36c83a0ed42a1f0
Oleg Kireev <okireev@ubic.tech>
2022-11-09 16:56:21 +0400

    @/app/App: wrap with TooltipProvider

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -3,19 +3,22 @@ import { TalentCalcProvider } from 'features/talent-calc/model/context';
 import {
   BrowserRouter, Route, Routes,
 } from 'react-router-dom';
+import { TooltipProvider } from 'shared/context/tooltip';
 import { Layout } from '../Layout';
 import styles from './styles.module.scss';
 
 export const App = () => (
-  <TalentCalcProvider>
-    <div className={styles.wrapper}>
-      <Layout>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<TalentCalc />} />
-          </Routes>
-        </BrowserRouter>
-      </Layout>
-    </div>
-  </TalentCalcProvider>
+  <TooltipProvider>
+    <TalentCalcProvider>
+      <div className={styles.wrapper}>
+        <Layout>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<TalentCalc />} />
+            </Routes>
+          </BrowserRouter>
+        </Layout>
+      </div>
+    </TalentCalcProvider>
+  </TooltipProvider>
 );
