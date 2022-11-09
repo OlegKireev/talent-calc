61848beaefb4f741a9bcfed915439802177a340a
Oleg Kireev <okireev@ubic.tech>
2022-11-09 11:29:00 +0400

    @/features/talent-calc/lib/utils: add getRequiredTalantPositions, generateArrowClass, checkHasChildrenTalentsNoValue

--- src/features/talent-calc/lib/utils.test.ts
+++ src/features/talent-calc/lib/utils.test.ts
@@ -1,12 +1,19 @@
 import { TALENTS_TEMPLATE } from 'mocks/talents';
 import { mageTalentsState } from 'mocks/talentsState';
+import { ArrowPositionType } from '../ui/SpecializationTrees/Tree/Arrow';
 import { TalentsStateType } from './transform';
 import {
+  checkHasChildrenTalentsNoValue,
   checkIsTalentsDataRefreshed,
   checkIsTierAvailable,
   checkRequiredTalent,
+  generateArrowClass,
   getDeepestTierWithValue,
-  getPreviousTiersTotal, getTierTotal, getTotalToUnblockNextTier, getTreeTotal,
+  getPreviousTiersTotal,
+  gerArrowPosition,
+  getTierTotal,
+  getTotalToUnblockNextTier,
+  getTreeTotal,
 } from './utils';
 
 describe('features/talent-calc/lib/utils:getTotalToUnblockNextTier()', () => {
@@ -203,5 +210,243 @@ describe('features/talent-calc/lib/utils:checkRequiredTalent()', () => {
 
   it('should a talent to be available if it doesn\'t have a "required" param', () => {
     expect(checkRequiredTalent(undefined, mockFrostMageState)).toBe(true);
+  });
+});
+
+describe('features/talent-calc/lib/utils:getRequiredTalantPositions', () => {
+  const frostMageTalents = TALENTS_TEMPLATE.mage[2].talents;
+
+  it('should return coords for a talent with a required talent', () => {
+    const expectedArrowCoords = { from: { column: 2, tier: 5 }, to: { column: 1, tier: 6 } };
+    const talentWithRequiredTalent = 'mage_frost_cold_as_ice';
+    const requiredTalent = 'mage_frost_cold_snap';
+
+    expect(gerArrowPosition(
+      talentWithRequiredTalent,
+      requiredTalent,
+      frostMageTalents,
+    )).toEqual(expectedArrowCoords);
+  });
+
+  it('should return "false" for a talent without a required talent', () => {
+    const talentWithoutRequiredTalent = 'mage_frost_fronst_warding';
+    const someRequredTalent = 'mage_frost_cold_snap';
+
+    expect(gerArrowPosition(
+      talentWithoutRequiredTalent,
+      someRequredTalent,
+      frostMageTalents,
+    )).toEqual(false);
+  });
+
+  it('should return "false" for an another class\' talent', () => {
+    const talentWithoutRequiredTalent = 'deathknight_blood_butchery';
+    const someRequredTalent = 'mage_frost_ice_barrier';
+
+    expect(gerArrowPosition(
+      talentWithoutRequiredTalent,
+      someRequredTalent,
+      frostMageTalents,
+    )).toEqual(false);
+  });
+});
+
+describe('features/talent-calc/lib/utils:generateArrowClass', () => {
+  it('should generate "down" correct', () => {
+    const downArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 5,
+      },
+      to: {
+        column: 2,
+        tier: 6,
+      },
+    };
+
+    expect(generateArrowClass(downArrowCoords)).toBe('down');
+  });
+
+  it('should generate "left" correct', () => {
+    const leftArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 5,
+      },
+      to: {
+        column: 1,
+        tier: 5,
+      },
+    };
+
+    expect(generateArrowClass(leftArrowCoords)).toBe('left');
+  });
+
+  it('should generate "right" correct', () => {
+    const rightArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 5,
+      },
+      to: {
+        column: 3,
+        tier: 5,
+      },
+    };
+
+    expect(generateArrowClass(rightArrowCoords)).toBe('right');
+  });
+
+  it('should generate "left-down" correct', () => {
+    const leftDownArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 5,
+      },
+      to: {
+        column: 1,
+        tier: 6,
+      },
+    };
+
+    expect(generateArrowClass(leftDownArrowCoords)).toBe('left-down');
+  });
+
+  it('should generate "right-down" correct', () => {
+    const rightDownArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 5,
+      },
+      to: {
+        column: 3,
+        tier: 6,
+      },
+    };
+
+    expect(generateArrowClass(rightDownArrowCoords)).toBe('right-down');
+  });
+
+  it('should generate correct values with more than one tier differnce', () => {
+    const downArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 3,
+        tier: 2,
+      },
+      to: {
+        column: 3,
+        tier: 5,
+      },
+    };
+
+    expect(generateArrowClass(downArrowCoords)).toBe('down');
+
+    const rightDownArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 2,
+      },
+      to: {
+        column: 3,
+        tier: 5,
+      },
+    };
+
+    expect(generateArrowClass(rightDownArrowCoords)).toBe('right-down');
+
+    const leftDownArrowCoords: {
+      from: ArrowPositionType,
+      to: ArrowPositionType
+    } = {
+      from: {
+        column: 2,
+        tier: 2,
+      },
+      to: {
+        column: 1,
+        tier: 5,
+      },
+    };
+
+    expect(generateArrowClass(leftDownArrowCoords)).toBe('left-down');
+  });
+});
+
+describe('features/talent-calc/lib/utils:checkHasChildrenTalentsNoValue', () => {
+  const frostMageTalents = TALENTS_TEMPLATE.mage[2].talents;
+  let mockFrostMageState: TalentsStateType = {};
+  beforeEach(() => {
+    mockFrostMageState = JSON.parse(JSON.stringify(mageTalentsState.frost));
+  });
+
+  it('should return "true" on an initial state', () => {
+    expect(checkHasChildrenTalentsNoValue(
+      'mage_frost_cold_snap',
+      frostMageTalents,
+      mockFrostMageState,
+    )).toBe(true);
+  });
+
+  it('should return "true" if only the current talent has value', () => {
+    mockFrostMageState.mage_frost_cold_snap = 1;
+
+    expect(checkHasChildrenTalentsNoValue(
+      'mage_frost_cold_snap',
+      frostMageTalents,
+      mockFrostMageState,
+    )).toBe(true);
+  });
+
+  it('should return "false" if a parent and a child has value ', () => {
+    mockFrostMageState.mage_frost_cold_snap = 1;
+    mockFrostMageState.mage_frost_cold_as_ice = 1;
+
+    expect(checkHasChildrenTalentsNoValue(
+      'mage_frost_cold_snap',
+      frostMageTalents,
+      mockFrostMageState,
+    )).toBe(false);
+  });
+
+  it('should return "false" if only a child has value', () => {
+    mockFrostMageState.mage_frost_cold_as_ice = 1;
+
+    expect(checkHasChildrenTalentsNoValue(
+      'mage_frost_cold_snap',
+      frostMageTalents,
+      mockFrostMageState,
+    )).toBe(false);
+  });
+
+  it('should return "false" if two children has value', () => {
+    mockFrostMageState.mage_frost_cold_as_ice = 2;
+    mockFrostMageState.mage_frost_ice_barrier = 1;
+
+    expect(checkHasChildrenTalentsNoValue(
+      'mage_frost_cold_snap',
+      frostMageTalents,
+      mockFrostMageState,
+    )).toBe(false);
   });
 });
