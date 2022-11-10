4543c1017bdbd8a9988a7735280e5070076ec406
Oleg Kireev <okireev@ubic.tech>
2022-11-10 10:12:14 +0400

    @/shared/ui/Tooltip: display type, desctiption

--- src/shared/ui/Tooltip/index.tsx
+++ src/shared/ui/Tooltip/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
+import type { TalentDescriptionType, TalentMaxValueType } from 'shared/constants/talentsData';
+import type { TooltipCoordsType, TooltipType } from './types';
 import styles from './styles.module.scss';
-import { TooltipCoordsType, TooltipType } from './types';
 
 const MIN_RANK_VALUE = 1;
 
@@ -14,16 +15,24 @@ export const Tooltip = ({
   data,
   coords,
 }: TooltipProps) => {
-  const { title } = data;
-  const [rank, setRank] = useState<number>(1);
+  const [level, setLevel] = useState<number>(0);
+  const [rank, setRank] = useState<TalentMaxValueType>(1);
+  const [description, setDescription] = useState<TalentDescriptionType>({ 1: '' });
+
+  const { type, title } = data;
+  const { x, y } = coords;
+
+  const isTalentType = type === 'talent';
+  const nextRankDescription = description[rank + 1 as TalentMaxValueType];
+  const shouldDisplayNextRankDescription = isTalentType && level > 0 && nextRankDescription;
 
   useEffect(() => {
     if ('rank' in data) {
-      setRank(Math.max(data.rank, MIN_RANK_VALUE));
+      setRank(Math.max(data.rank, MIN_RANK_VALUE) as TalentMaxValueType);
+      setLevel(data.rank);
+      setDescription(data.description);
     }
   }, [data]);
-
-  const { x, y } = coords;
 
   return createPortal(
     (
@@ -46,6 +55,30 @@ export const Tooltip = ({
             </span>
           )}
         </header>
+        {isTalentType && (
+          <span className={styles.type}>Talent</span>
+        )}
+        {isTalentType && (
+          <p className={styles.desc}>
+            {description[rank]}
+          </p>
+        )}
+        {shouldDisplayNextRankDescription && (
+          <>
+            <span
+              className={styles.label}
+              style={{
+                display: 'block',
+                marginTop: '.5rem',
+              }}
+            >
+              Next rank:
+            </span>
+            <p className={styles.desc}>
+              {nextRankDescription}
+            </p>
+          </>
+        )}
       </div>
     ),
     document.body,
