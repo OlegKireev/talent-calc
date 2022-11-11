bf68833a3eb7a2a1d0c6fb3f8dc7433265714970
Oleg Kireev <okireev@ubic.tech>
2022-11-11 17:53:53 +0400

    @/features/talent-calc/lib/transform: create generateAllTalents

--- src/features/talent-calc/lib/transform.ts
+++ src/features/talent-calc/lib/transform.ts
@@ -1,6 +1,6 @@
-import { type CharacterSpecializationType } from 'shared/constants/global';
+import { CharacterClassType, type CharacterSpecializationType } from 'shared/constants/global';
 import { type CharacterTalentIdType } from 'shared/constants/talents';
-import { type TaletsOfClassType } from 'shared/constants/talentsData';
+import { TalentsType, type TaletsOfClassType } from 'shared/constants/talentsData';
 
 export const createTalentsState = (
   obj: TaletsOfClassType[],
@@ -15,4 +15,14 @@ export const createTalentsState = (
 export type TalentsStateType = { [key in CharacterTalentIdType]?: number };
 export type CreateTaletsStateReturn = {
   [key in CharacterSpecializationType]?: TalentsStateType
+};
+
+export const generateAllTalents = (talents: TalentsType) => {
+  const classes = Object.keys(talents) as CharacterClassType[];
+
+  return classes
+    .map((characterClass) => talents[characterClass])
+    .flat()
+    .map((t) => t.talents)
+    .flat();
 };
