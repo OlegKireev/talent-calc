b42e076ae69065df554db3e3fe23859e74d15bc2
Oleg Kireev <okireev@ubic.tech>
2022-11-11 17:53:35 +0400

    @/features/talent-calc: move talents state to context

--- src/features/talent-calc/index.tsx
+++ src/features/talent-calc/index.tsx
@@ -1,46 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { TALENTS_TEMPLATE } from 'mocks/talents';
 import { ClassChoser } from './ui/ClassChoser';
 import { Controls } from './ui/Controls';
 import { SpecializationTrees } from './ui/SpecializationTrees';
-import { createTalentsState, type CreateTaletsStateReturn } from './lib/transform';
+import { createTalentsState } from './lib/transform';
 import { useTalentCalcContext } from './model/context';
 import { checkIsTalentsDataRefreshed } from './lib/utils';
 import { type HandleTalentChange } from './types';
 import styles from './styles.module.scss';
 
 export const TalentCalc = () => {
-  const { currentClass, onClassChange } = useTalentCalcContext();
-  const [state, setState] = useState<CreateTaletsStateReturn>({});
+  const {
+    currentClass,
+    state,
+    setState,
+    setClass,
+    setTalents,
+  } = useTalentCalcContext();
+
+  const isDataRefreshed = checkIsTalentsDataRefreshed(currentClass, state);
+
+  useEffect(() => {
+    setTalents(TALENTS_TEMPLATE);
+  }, [setTalents]);
 
   useEffect(() => {
     if (!currentClass || !TALENTS_TEMPLATE[currentClass]) {
       return;
     }
     setState(createTalentsState(TALENTS_TEMPLATE[currentClass]));
-  }, [currentClass]);
+  }, [currentClass, setState]);
 
   const handleTalentChange: HandleTalentChange = ({
     specialization,
     id,
     value,
   }) => {
-    setState((prev) => ({
-      ...prev,
-      [specialization]: {
-        ...prev[specialization],
-        [id]: value,
-      },
-    }));
+    if (state) {
+      setState({
+        ...state,
+        [specialization]: {
+          ...state[specialization],
+          [id]: value,
+        },
+      });
+    }
   };
-
-  const isDataRefreshed = checkIsTalentsDataRefreshed(currentClass, state);
 
   return (
     <div className={styles.wrapper}>
       <ClassChoser
         currentClass={currentClass}
-        onClassChange={onClassChange}
+        onClassChange={setClass}
       />
       {currentClass && TALENTS_TEMPLATE[currentClass] && isDataRefreshed ? (
         <SpecializationTrees
