aa28ca80e705359442442b259c4965d09b18049f
Oleg Kireev <okireev@ubic.tech>
2022-11-12 20:33:29 +0400

    @/features/talent-calc/ui/ClassChoser: wrap buttons with Link

--- src/features/talent-calc/ui/ClassChoser/index.tsx
+++ src/features/talent-calc/ui/ClassChoser/index.tsx
@@ -1,25 +1,25 @@
+import { Link } from 'react-router-dom';
 import { characterClasses, RESOURCE_URI, type CharacterClassType } from 'shared/constants/global';
 import { AbilityButton } from 'shared/ui/AbilityButton';
 import styles from './styles.module.scss';
 
 interface ClassChoserProps {
   currentClass: CharacterClassType | null,
-  onClassChange: (newClass: CharacterClassType) => void,
 }
 
 export const ClassChoser = ({
   currentClass,
-  onClassChange,
 }: ClassChoserProps) => (
   <div className={styles.wrapper}>
     {characterClasses.map((characterClass) => (
-      <AbilityButton
-        key={characterClass}
-        isActive={characterClass === currentClass}
-        isDisabled={false}
-        background={`${RESOURCE_URI}/icons/large/class_${characterClass}.jpg`}
-        onClick={() => onClassChange(characterClass)}
-      />
+      <Link to={`/${characterClass}`}>
+        <AbilityButton
+          key={characterClass}
+          isSelected={characterClass === currentClass}
+          isDimmed={characterClass !== currentClass}
+          background={`${RESOURCE_URI}/icons/large/class_${characterClass}.jpg`}
+        />
+      </Link>
     ))}
   </div>
 );
