165331906bfd951cbe995ac0f8595037c94884ee
Oleg Kireev <okireev@ubic.tech>
2022-11-15 00:15:01 +0400

    @/features/talent-calc/ui/Status: display dynamic data

--- src/features/talent-calc/ui/Status/index.tsx
+++ src/features/talent-calc/ui/Status/index.tsx
@@ -1,50 +1,62 @@
-import { CharacterClassType, CHARACTER_COLORS } from 'shared/constants/global';
+import {
+  type CharacterClassType, CHARACTER_COLORS, FIRST_TALTNT_CHARACTER_LEVEL, MAX_TALENTS_POINTS,
+} from 'shared/constants/global';
 import cx from 'classnames';
 import styles from './styles.module.scss';
 
 interface StatusProps {
   currentClass: CharacterClassType
   specsTotal: number[]
+  total: number
 }
 
 export const Status = ({
   currentClass,
   specsTotal,
-}: StatusProps) => (
-  <div className={styles.wrapper}>
-    <span
-      className={styles.class}
-      style={{ color: CHARACTER_COLORS[currentClass] }}
-    >
-      {currentClass}
-      :
-    </span>
-    <span className={styles.state}>
-      <span className={cx({ [styles.bold]: Boolean(specsTotal[0]) })}>
-        {specsTotal[0]}
+  total,
+}: StatusProps) => {
+  const pointsLeft = MAX_TALENTS_POINTS - total;
+  const requiedLevel = FIRST_TALTNT_CHARACTER_LEVEL - 1 + total;
+  const shouldRenderRequiredLevel = requiedLevel >= FIRST_TALTNT_CHARACTER_LEVEL;
+
+  return (
+    <div className={styles.wrapper}>
+      <span
+        className={styles.class}
+        style={{ color: CHARACTER_COLORS[currentClass] }}
+      >
+        {currentClass}
+        :
       </span>
-      {' '}
-      /
-      {' '}
-      <span className={cx({ [styles.bold]: Boolean(specsTotal[1]) })}>
-        {specsTotal[1]}
+      <span className={styles.state}>
+        <span className={cx({ [styles.bold]: Boolean(specsTotal[0]) })}>
+          {specsTotal[0]}
+        </span>
+        {' '}
+        /
+        {' '}
+        <span className={cx({ [styles.bold]: Boolean(specsTotal[1]) })}>
+          {specsTotal[1]}
+        </span>
+        {' '}
+        /
+        {' '}
+        <span className={cx({ [styles.bold]: Boolean(specsTotal[2]) })}>
+          {specsTotal[2]}
+        </span>
       </span>
-      {' '}
-      /
-      {' '}
-      <span className={cx({ [styles.bold]: Boolean(specsTotal[2]) })}>
-        {specsTotal[2]}
+      {shouldRenderRequiredLevel && (
+        <span className={styles.property}>
+          Required level:
+          {' '}
+          <strong>{requiedLevel}</strong>
+        </span>
+      )}
+      <span className={styles.property}>
+        Points left:
+        {' '}
+        <strong>{pointsLeft}</strong>
       </span>
-    </span>
-    <span className={styles.property}>
-      Required level:
-      {' '}
-      <strong>55</strong>
-    </span>
-    <span className={styles.property}>
-      Points left:
-      {' '}
-      <strong>70</strong>
-    </span>
-  </div>
-);
+    </div>
+  );
+};
