dfcb5d468a865956e820e8eb472b9e6c347dba22
Oleg Kireev <okireev@ubic.tech>
2022-12-03 11:08:19 +0400

    @/shared/ui/Tooltip/utils: create getTooltipCoords

---
+++ src/shared/ui/Tooltip/utils.ts
@@ -0,0 +1,25 @@
+import { RefObject } from 'react';
+import { TooltipCoordsType } from './types';
+
+export const getTooltipCoords = (targetCoords: TooltipCoordsType, ref: RefObject<HTMLElement>) => {
+  const DEFAULT_MARGIN = 10;
+
+  const { x } = targetCoords;
+  let { y } = targetCoords;
+
+  if (ref.current) {
+    const { height: tooltipHeight } = ref.current.getBoundingClientRect();
+    const { innerHeight: windowHeight } = window;
+
+    if (y + tooltipHeight > windowHeight) {
+      y = windowHeight - tooltipHeight - DEFAULT_MARGIN;
+    } else if (y < DEFAULT_MARGIN) {
+      y = DEFAULT_MARGIN;
+    }
+  }
+
+  return {
+    x,
+    y,
+  };
+};
