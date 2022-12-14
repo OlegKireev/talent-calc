ad499e26943257b4a5c72fe173ee557f2b54f7f9
Oleg Kireev <okireev@ubic.tech>
2022-12-14 10:00:29 +0400

    @/app/App: remove wrapper

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -5,21 +5,18 @@ import { TalentCalc } from 'features/talent-calc';
 import { TalentCalcProvider } from 'features/talent-calc/model/context';
 import { TooltipProvider } from 'shared/context/tooltip';
 import { Layout } from '../Layout';
-import styles from './styles.module.scss';
 
 export const App = () => (
   <TooltipProvider>
     <TalentCalcProvider>
-      <div className={styles.wrapper}>
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<TalentCalc />} />
-              <Route path=":characterClass" element={<TalentCalc />} />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<TalentCalc />} />
+            <Route path=":characterClass" element={<TalentCalc />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </TalentCalcProvider>
   </TooltipProvider>
 );
