a148e00e3df6c3f75801a76c2b0033252a4344f2
Oleg Kireev <okireev@ubic.tech>
2022-12-22 10:27:37 +0400

    @/app/App: return wrapper

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -5,17 +5,20 @@ import { TalentCalc } from 'features/talent-calc';
 import { TalentCalcProvider } from 'features/talent-calc/model/context';
 import { TooltipProvider } from 'shared/context/tooltip';
 import { Layout } from '../Layout';
+import styles from './styles.module.scss';
 
 export const App = () => (
   <TooltipProvider>
     <TalentCalcProvider>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<TalentCalc />} />
-            <Route path=":characterClass" element={<TalentCalc />} />
-          </Routes>
-        </Layout>
+        <div className={styles.wrapper}>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<TalentCalc />} />
+              <Route path=":characterClass" element={<TalentCalc />} />
+            </Routes>
+          </Layout>
+        </div>
       </BrowserRouter>
     </TalentCalcProvider>
   </TooltipProvider>
